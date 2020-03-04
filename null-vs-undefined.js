b780ee52417cc049d29caa353354211d89c176f1
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-05 02:01:05 +0600

    we can get undefined many way but null value developer set own purpose

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi)
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = add(13, 42);
+console.log(result);
+
+function add2(num1, num2){
+    console.log(num2);
+}
+const result2 = add2(13);
+
+const premikPurus = {
+    name:  "smart dude",
+    phone: 23525221
+}
+console.log(premikPurus.gf);
+
+let arr = [23, 23, 5];
+
+console.log(arr[23]);
