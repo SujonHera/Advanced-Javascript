97b123ed0abcea00ac98b87d7c595166be85eecb
SujonHera <sujon.hossain758@gmail.com>
2020-03-06 01:43:50 +0600

    we create a json file for practices it is interesting subject

--- json.js
+++ json.js
@@ -33,10 +33,10 @@
 
 
 const friends = [
-    {name: "Olil", phone: 1234123},
-    {name: "Jolil", phone: 3523523},
-    {name: "Kholil", phone: 3532123},
-    {name: "Molil", phone: 2353252}
+    {fdname: "Olil", phone: 1234123},
+    {fdname: "Jolil", phone: 3523523},
+    {fdname: "Kholil", phone: 3532123},
+    {fdname: "Molil", phone: 2353252}
 ]
 
 const friendsJSON = JSON.stringify(friends);
@@ -44,5 +44,5 @@ const friendsDataFromJSON = JSON.parse(friendsJSON);
 
 console.log(friendsDataFromJSON);
 
-const friendsName = friendsDataFromJSON.map(name => name.name);
+const friendsName = friendsDataFromJSON.map(name => name.fdname);
 console.log(friendsName)
